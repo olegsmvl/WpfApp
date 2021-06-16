4ee1a2d462b09d73a4ebb4fa381a35e24a4d096a
olegsmvl <33567178+olegsmvl@users.noreply.github.com>
2021-06-16 23:56:33 +0300

    add maker class

---
+++ WpfApp/Maker.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Collections.ObjectModel;
+using WpfApp.Model;
+using WpfApp.Controller;
+
+namespace WpfApp
+{
+    class Maker
+    {
+        public ObservableCollection<Record> Records { get; set; }
+
+        private DbManager DbManager;
+        private const int TEXT_LENGTH_ITEM = 6;
+
+        public Maker()
+        {
+            DbManager = new DbManager();
+            UpdateComboBoxAsync();
+        }
+
+        private async void UpdateComboBoxAsync()
+        {
+            var records = await DbManager.LoadData();
+            //comboBox.ItemsSource = records.Select(x => BuildItem(x));
+        }
+
+        private string BuildItem(Record record)
+        {
+            if (record.Text.Length < TEXT_LENGTH_ITEM)
+                return $"{record.Id} \"{record.Text.Substring(0, record.Text.Length)}\" {record.Log.DateTime}";
+
+            return $"{record.Id} \"{record.Text.Substring(0, TEXT_LENGTH_ITEM)}\" {record.Log.DateTime}";
+        }
+    }
+}
